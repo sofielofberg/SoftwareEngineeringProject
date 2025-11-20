4e137ba0d6b05f75952994e7ee60b813a77fc29e
Olivia <olivia.famjespersen@gmail.com>
2025-11-20 13:47:48 +0100

    Move database outside of main into its own module

--- main.py
+++ main.py
@@ -3,39 +3,23 @@
 
 from flask import Flask
 from flask_login import LoginManager
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase, MappedAsDataclass
+
+import database
+import user
 
 # Start flask
 app = Flask(__name__)
 app.secret_key = "SECRET TUNNEL!!"
 
-# Database stuff
-class Base(DeclarativeBase, MappedAsDataclass):
-    pass
-
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
-
-db = SQLAlchemy(app, model_class=Base)
-
-# Setup the database
-import accountant
-import admin
-import manager
-import receipt
-import salesman
-import user
-
-with app.app_context():
-    db.drop_all()
-    db.create_all()
-    db.session.add(manager.Manager("admin", "admin"))
-    db.session.commit()
+# Initialize database
+database.init_db(app)
+database.setup_db(app)
 
 # Login
 login_manager = LoginManager(app)
 login_manager.session_protection = "strong"
 login_manager.login_view = "login"
+
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -43,4 +27,4 @@ def load_user(user_id):
 
 
 # Import the views
-import views
+import views  # noqa: F401
