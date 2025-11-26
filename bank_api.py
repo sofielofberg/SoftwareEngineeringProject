c86abd035c90535c1c6bcf09c0f7c0e317cb508b
Jonathan Kilhof <Jonathan@kilhof.com>
2025-11-26 14:59:06 +0100

    added bank api proof of concept

---
+++ bank_api.py
@@ -0,0 +1,22 @@
+"""
+Proof of concept for bank api
+"""
+import pprint
+import requests
+
+get_response = requests.get("http://finance.lutzen.dk/api/v1/customers",
+                            headers={"accept":"application/vnd.api+json",
+                                     "Authorization":"Bearer rTouTYOgn4qXxGhAnBzW59CeKaYGTWJWIAhX8CRv"})
+#print(get_response.status_code)
+
+#pprint.pprint(get_response.json())
+
+patch_response = requests.get("https://finance.lutzen.dk/api/v1/payments/51",
+                               headers={"accept":"application/vnd.api+json",
+                                        "Authorization":"Bearer rTouTYOgn4qXxGhAnBzW59CeKaYGTWJWIAhX8CRv",
+                                        "Content-Type":"application/vnd.api+json"},
+                               data={"data": {"type": "payments",
+                                              "id": "1337",
+                                              "attributes": {"status": "pending"}}})
+#print(patch_response.status_code)
+#pprint.pprint(patch_response.json())
