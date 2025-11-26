7986d3281336f540edd30e7bc046d226532345a7
Jonathan Kilhof <Jonathan@kilhof.com>
2025-11-26 14:59:48 +0100

    .

--- bank_api.py
+++ bank_api.py
@@ -8,7 +8,6 @@ get_response = requests.get("http://finance.lutzen.dk/api/v1/customers",
                             headers={"accept":"application/vnd.api+json",
                                      "Authorization":"Bearer rTouTYOgn4qXxGhAnBzW59CeKaYGTWJWIAhX8CRv"})
 #print(get_response.status_code)
-
 #pprint.pprint(get_response.json())
 
 patch_response = requests.get("https://finance.lutzen.dk/api/v1/payments/51",
